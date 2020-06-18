4d3c7c73518386c435d76421d9654e1bbe608b34
Miguel <miguelangelmal@gmail.com>
2020-06-18 18:14:08 +0200

    Initial commit

---
+++ display.js
@@ -0,0 +1,32 @@
+const SCALE = 20;
+function createElement(type, className){
+    let element = document.createElement(type);
+    if (className){
+        element.className = className;
+    }
+    return element;
+}
+
+function DOMDisplay (parent, level){
+    this.wrap = parent.appendChild(createElement('div', 'game'));
+    this.level = level;
+
+    
+
+    //this.wrap.appendChild();
+    this.drawBackground = function () {
+        let table = createElement('table', 'background');
+        table.style.width = this.level.width*SCALE + 'px';
+
+        this.level.grid.forEach(row => {
+            let rowElement = createElement("tr");
+            rowElement.style.height = SCALE + 'px';
+            table.appendChild(rowElement);
+            row.forEach(type => rowElement.appendChild(createElement('td', type)));
+        });
+
+        return table;
+    }
+
+    this.wrap.appendChild(this.drawBackground());
+}
