19752aad1ed9e0b0740e496af8dafcf44870914e
Miguel <miguelangelmal@gmail.com>
2020-06-20 00:40:09 +0200

    Add game logic

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ function trackKeys (keyCodes) {
         if (keyCodes.hasOwnProperty(event.keyCode)){
             let downPressed = event.type === 'keydown';
             pressedKeys[keyCodes[event.keyCode]] = downPressed;
+            event.preventDefault();
         }
     }
 
@@ -20,5 +21,47 @@ function trackKeys (keyCodes) {
     addEventListener('keyup', handler);
     return pressedKeys;
 }
-let level = new Level(GAME_LEVELS);
-let display = new DOMDisplay(document.body, level);
+
+function runAnimation(frameFunction){
+    let lastTime = null;
+    function frame (time){
+        let stop = false;
+        if (lastTime !== null){
+            let timeStep = Math.min(time - lastTime, 100) / 1000; 
+            stop = frameFunction(timeStep) === false;
+        }
+        lastTime = time;
+        if (!stop){
+            requestAnimationFrame(frame);
+        }
+    }
+    requestAnimationFrame(frame);
+}
+
+function runLevel (level, Display, callback){
+    let display = new Display(document.body, level);
+    runAnimation(function (step){
+        display.drawFrame();
+
+        if (level.isFinished()){
+            display.clear();
+            if (callback)
+                callback(level.status);
+            return false;
+        }
+    });
+}
+
+
+function runGame(level, Display){
+    let levelObject = new Level(level);
+    runLevel(levelObject, Display, status => {
+        if (status === 'lost') 
+            console.log('Has perdido');
+        else
+            console.log('Has ganado !!!') 
+    
+    });
+        
+}
+runGame(GAME_LEVELS, DOMDisplay);
