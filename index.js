22f837b28cd86b7a2cd7592055985659344207f7
Miguel <miguelangelmal@gmail.com>
2020-06-20 00:24:20 +0200

    Add event listener to keys

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
+const ARROW_CODES = {
+    37: 'left',
+    38: 'up',
+    39: 'right'
+}
+
+let arrows = trackKeys(ARROW_CODES);
+
+function trackKeys (keyCodes) {
+    let pressedKeys = {};
+
+    function handler(event){
+        if (keyCodes.hasOwnProperty(event.keyCode)){
+            let downPressed = event.type === 'keydown';
+            pressedKeys[keyCodes[event.keyCode]] = downPressed;
+        }
+    }
+
+    addEventListener('keydown',  handler);
+    addEventListener('keyup', handler);
+    return pressedKeys;
+}
 let level = new Level(GAME_LEVELS);
 let display = new DOMDisplay(document.body, level);
